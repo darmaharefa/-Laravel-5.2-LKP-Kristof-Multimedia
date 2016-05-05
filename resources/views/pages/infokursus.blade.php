7b5095d736fd56a12c1793e01501bf16ee47db56
Darma Kurniawan Harefa <darmakurniawanharefa@gmail.com>
2016-05-05 13:29:17 +0700

    Perbaiki #info-kursus Detail Aplikasi Perkantoran

--- resources/views/pages/infokursus.blade.php
+++ resources/views/pages/infokursus.blade.php
@@ -16,13 +16,12 @@
 		<p>Program atau materi kursus yang dilaksanakan KRISTOF MULTIMEDIA adalah program aplikasi berbasis komputer yang dapat dipergunakan secara langsung oleh peserta dan dapat diinterprestasikan dalam berbagai bentuk pekerjaan baik dibidang pemerintahan maupun swasta. Program-program tersebut adalah :</p>
 		<hr>
 		<div class="col-md-6">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1"><img src="images/4.jpg" class="img-responsive"></div>
+			<div class="row well">
+				<div class="col-md-10 col-md-offset-1"><img src="images/aplikasi-perkantoran.png" class="img-responsive"></div>
 			</div>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
-			libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
-			libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque
-			libero, pulvinar tincidunt leo consectetur eget. Curabitur lacinia pellentesque</p>
+			<hr>
+			<p>Paket Aplikasi Perkantoran yang akan diajarkan adalah Microsoft Office. Microsoft Office adalah perangkat lunak paket aplikasi perkantoran buatan Microsoft dan dirancang untuk dijalankan di bawah sistem operasi Microsoft Windows dan Mac OS X. Beberapa aplikasi di dalam Microsoft Office yang terkenal adalah Excel, Word, dan PowerPoint.</p>	
+			<p>Pemilihan Aplikasi Microsoft Office dalam pembelajaran Aplikasi Perkantoran salah satunya adalah karena kemudahan dalam penggunaannya. Selain kemudahannya, Perkantoran saat ini masih banyak yang menggunakan Microsoft-Windows sebagai Sistem Operasinya, sehingga pengetahuan akan Microsoft-Office sebagai Aplikasi Perkantoran akan menjadi modol yang besar.</p>
 		</div>
 		
 		<div class="col-md-6">
@@ -35,8 +34,7 @@
 						</div>
 						<div class="media-body">
 							<h4 class="media-heading">Microsoft Office Word</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
-							libero, pulvinar tincidunt leo consectetur eget.</p>
+							<p>Microsoft Word merupakan Aplikasi pengelola kata, contoh file yang bisa dibuat dengan Microsoft Word yaitu : surat, laporan, makalah, artikel, tabel, skripsi dan lain-lain</p>
 						</div>
 					</li>
 					<li>
@@ -45,8 +43,7 @@
 						</div>
 						<div class="media-body">
 							<h4 class="media-heading">Microsoft Office Excel</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
-							libero, pulvinar tincidunt leo consectetur eget.</p>
+							<p>Microsoft Excel merupakan aplikasi pengolah angka/data dan disajikan dalam bentuk tabel atau grafik, aplikasi menyediakan fitur rumus yang dapat melakukan perhitungan secara otomatis, biasanya aplikasi ini digunakan oleh akuntan dalam membuat laporan keuangan.</p>
 						</div>
 					</li>
 					<li>
@@ -55,8 +52,7 @@
 						</div>
 						<div class="media-body">
 							<h4 class="media-heading">Microsoft Office Power Point</h4>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum erat 
-							libero, pulvinar tincidunt leo consectetur eget.</p>
+							<p>Microsoft Powerpoint merupakan aplikasi yang berfungsi untuk membuat slide presentasi, dimana slide presentasi tersebut dapat ditampilkan melalui infokus dan dapat diprint dalam bentuk hangout, aplikasi ini juga menyediakan fitur animasi yang dapat membuat presentasi anda menjadi lebih menarik.</p>
 						</div>
 					</li>
 				</ul>
